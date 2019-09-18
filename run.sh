e4512e183c6f716b4c6dc9077c632c427dca6724
jmch1692 <45401057+jmch1692@users.noreply.github.com>
2019-09-18 14:10:54 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,16 +6,10 @@ if [ "${1:0:1}" != '-' ]; then
   exec "$@"
 fi
 
-exec 
-sysctl -w vm.max_map_count=262144 \
-sysctl -w fs.file-max=65536 \
-ulimit -n 65536 \
-ulimit -u 4096
-
 exec java -jar lib/sonar-application-$SONAR_VERSION.jar \
   -Dsonar.log.console=true \
   -Dsonar.jdbc.username="$SONARQUBE_JDBC_USERNAME" \
   -Dsonar.jdbc.password="$SONARQUBE_JDBC_PASSWORD" \
   -Dsonar.jdbc.url="$SONARQUBE_JDBC_URL" \
-  -Dsonar.web.javaAdditionalOpts="$SONARQUBE_WEB_JVM_OPTS -Djava.security.egd=file:/dev/./urandom -Dnode.store.allow_mmapfs=false" \
+  -Dsonar.web.javaAdditionalOpts="$SONARQUBE_WEB_JVM_OPTS -Djava.security.egd=file:/dev/./urandom" \
   "$@"
